b3d4f49b0886043247b982e07aa4a647fa6a69d9
HOÀNG ĐÌNH LONG <hoanglonggg356@gmail.com>
2025-08-30 23:23:51 +0700

    updated

--- src/components/WorkflowSection.tsx
+++ src/components/WorkflowSection.tsx
@@ -11,41 +11,41 @@ export default function WorkflowSection() {
             className="object-cover lg:w-[200px] md:w-[150px] w-[100px] m-auto"
           />
         </div>
-        <div className="absolute 2xl:right-[12%] xl:right-[4%] xl:top-[-3.5%] lg:top-[-4%] lg:right-[-1%] md:top-[-4.5%] md:right-[7%] sm:top-[-28px] top-[-3%] sm:right-[8%] right-[3%] xl:w-[500px] lg:w-[450px] md:w-[360px] sm:w-[250px] w-[160px] flex items-center lg:gap-4 md:gap-3 gap-2 border border-[#2427BD] md:rounded-[35px] rounded-2xl  md:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:left-[-35px] after:text-4xl after:text-blue-900 after:border-l-[35px] after:border-t-4">
+        <div className="absolute 2xl:right-[12%] xl:right-[4%] xl:top-[-3.5%] lg:top-[-4%] lg:right-[-1%] md:top-[-3%] md:right-[3%] sm:top-[-28px] top-[-3%] sm:right-[8%] right-[3%] xl:w-[500px] lg:w-[450px] md:w-[310px] sm:w-[250px] w-[160px] flex items-center lg:gap-4 md:gap-3 gap-2 border border-[#2427BD] md:rounded-[35px] rounded-2xl lg:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:left-[-35px] after:text-4xl after:text-blue-900 after:border-l-[35px] after:border-t-4">
           <span className="text-[#243761] font-bold lg:text-[45px] md:text-[33px] sm:text-xl text-base xl:text-[55px] md:mr-2">
             2025
           </span>
 
-          <p className="font-normal lg:text-[22px] md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[24px] sm:leading-[22px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
+          <p className="font-normal lg:text-[22px] md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[18px] sm:leading-[16px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
             We follow industry-standard methodologies and employ effective
             communication
           </p>
         </div>
-        <div className="absolute 2xl:right-[15%] xl:right-[8%] lg:bottom-[35.5%] lg:right-[5%] md:bottom-[34%] sm:bottom-[33.1%] md:right-[12%] right-[11%] bottom-[35%] xl:w-[500px] lg:w-[450px] md:w-[360px] sm:w-[250px] w-[160px] flex items-center gap-4 border border-[#2427BD] md:rounded-[35px] rounded-2xl md:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:left-[-35px] after:text-4xl after:text-blue-900 after:border-l-[35px] after:border-t-4">
+        <div className="absolute 2xl:right-[15%] xl:right-[8%] lg:bottom-[35.5%] lg:right-[5%] md:bottom-[35%] sm:bottom-[33.1%] md:right-[9%] right-[11%] bottom-[35%] xl:w-[500px] lg:w-[450px] md:w-[310px] sm:w-[250px] w-[160px] flex items-center lg:gap-4 md:gap-3 gap-2 border border-[#2427BD] md:rounded-[35px] rounded-2xl lg:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:left-[-35px] after:text-4xl after:text-blue-900 after:border-l-[35px] after:border-t-4">
           <span className="text-[#243761] font-bold lg:text-[45px] md:text-[33px] sm:text-xl text-base xl:text-[55px] md:mr-2">
             2023
           </span>
 
-          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[24px] sm:leading-[22px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
+          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[18px] sm:leading-[16px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
             We follow industry-standard methodologies and employ effective
             communication
           </p>
         </div>
-        <div className="absolute 2xl:left-[16%] lg:top-[29%] md:top-[27.5%] sm:top-[26%] top-[29%] xl:left-[7%] lg:left-[6%] md:left-[12.5%] sm:left-[13%]  left-[11%] xl:w-[500px] lg:w-[450px] md:w-[360px] sm:w-[250px] w-[160px] flex items-center gap-4 border border-[#2427BD] md:rounded-[35px] rounded-2xl md:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:right-[-35px] after:text-4xl after:text-blue-900 after:border-r-[35px] after:border-t-4">
+        <div className="absolute 2xl:left-[16%] lg:top-[29%] md:top-[28.5%] sm:top-[26%] top-[29%] xl:left-[7%] lg:left-[6%] md:left-[9.5%] sm:left-[13%] left-[11%] xl:w-[500px] lg:w-[450px] md:w-[310px] sm:w-[250px] w-[160px] flex items-center lg:gap-4 md:gap-3 gap-2 border border-[#2427BD] md:rounded-[35px] rounded-2xl lg:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:right-[-35px] after:text-4xl after:text-blue-900 after:border-r-[35px] after:border-t-4">
           <span className="text-[#243761] font-bold lg:text-[45px] md:text-[33px] sm:text-xl text-base xl:text-[55px] md:mr-2">
             2024
           </span>
 
-          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[24px] sm:leading-[22px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
+          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[18px] sm:leading-[16px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
             We follow industry-standard methodologies and employ effective
           </p>
         </div>
-        <div className="absolute 2xl:left-[13%] xl:left-[5%] xl:bottom-0 lg:bottom-[-1%] md:bottom-[-2%] sm:bottom-[-3%] bottom-[-1%] opacity-65 lg:left-[1%] md:left-[8.5%] sm:left-[8%] left-[7%] xl:w-[500px] lg:w-[450px] md:w-[360px] sm:w-[250px] w-[160px] flex items-center gap-4 border border-[#2427BD] md:rounded-[35px] rounded-2xl md:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:right-[-35px] after:text-4xl after:text-blue-900 after:border-r-[35px] after:border-t-4">
+        <div className="absolute 2xl:left-[13%] xl:left-[5%] xl:bottom-0 lg:bottom-[-1%] md:bottom-[-1%] sm:bottom-[-3%] bottom-[-1%] opacity-65 lg:left-[1%] md:left-[5%] sm:left-[8%] left-[7%] xl:w-[500px] lg:w-[450px] md:w-[310px] sm:w-[250px] w-[160px] flex items-center lg:gap-4 md:gap-3 gap-2 border border-[#2427BD] md:rounded-[35px] rounded-2xl lg:px-6 px-4 md:py-4 py-2 bg-white shadow-[0px_8px_3.4px_-3px_#67BBFF82] after:content-[' '] after:absolute after:right-[-35px] after:text-4xl after:text-blue-900 after:border-r-[35px] after:border-t-4">
           <span className="text-[#243761] font-bold lg:text-[45px] md:text-[33px] sm:text-xl text-base xl:text-[55px] md:mr-2">
             2022
           </span>
 
-          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[24px] sm:leading-[22px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
+          <p className="font-normal lg:text-[22px]  md:text-sm sm:text-xs text-[7px] lg:leading-[28px] md:leading-[18px] sm:leading-[16px] text-start text-[#243761] md:line-clamp-3 line-clamp-4">
             We follow industry-standard methodologies and employ effective
             communication
           </p>
